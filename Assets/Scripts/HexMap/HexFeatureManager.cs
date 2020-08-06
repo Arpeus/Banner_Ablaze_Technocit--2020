6c0c101bf8be73377dcd49c0829d20367f1b7f97
Arpeus <56725663+Arpeus@users.noreply.github.com>
2020-08-06 12:04:34 +0200

    Add Kenney prefab

---
+++ Assets/Scripts/HexMap/HexFeatureManager.cs
@@ -0,0 +1,24 @@
+﻿
+using UnityEngine;
+
+public class HexFeatureManager : MonoBehaviour
+{
+	public Transform featurePrefab;
+
+	public void Clear() 
+	{
+
+	}
+
+	public void Apply() 
+	{
+
+	}
+
+
+	public void AddFeature(Vector3 position) 
+	{
+		Transform instance = Instantiate(featurePrefab);
+		instance.localPosition = position;
+	}
+}
