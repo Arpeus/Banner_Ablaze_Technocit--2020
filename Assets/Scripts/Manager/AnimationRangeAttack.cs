0aa6e430c895c0f64db464e20908fc03cad6aaad
Allard Mederic <medericallardisf@gmail.com>
2020-08-31 10:19:34 +0200

    scene animation range

--- Assets/Scripts/Manager/AnimationRangeAttack.cs
+++ Assets/Scripts/Manager/AnimationRangeAttack.cs
@@ -2,17 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AnimationRangeDefense : AnimationDefense
+public class AnimationRangeAttack : AnimationDefense
 {
     [SerializeField] private GameObject m_placeDamageAttack;
-    
     public GameObject prefabFx;
+    //[SerializeField] private Animator m_animatorDamage;
 
     // Start is called before the first frame update
     protected override void Start()
     {
-        m_placeDamageAttack = HUDInGame.Instance._placeDefenseRangeUnits[index % 3];
-      
+        m_placeDamageAttack = HUDInGame.Instance._placeAttackRangeUnits[index % 3];
+        //m_animatorDamage = m_placeDamageAttack.GetComponent<Animator>();
         base.Start();
     }
 
