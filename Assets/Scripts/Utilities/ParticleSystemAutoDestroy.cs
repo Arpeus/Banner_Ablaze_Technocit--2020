8467e62ee031b8507b3a499cca00589d76a6dd8b
Allard Mederic <medericallardisf@gmail.com>
2020-09-03 14:26:03 +0200

    HealParticles

---
+++ Assets/Scripts/Utilities/ParticleSystemAutoDestroy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ParticleSystemAutoDestroy : MonoBehaviour
+{
+    private void Start()
+    {
+        StartCoroutine(StartDestroy());
+    }
+
+    private IEnumerator StartDestroy()
+    {
+        yield return new WaitForSeconds(GetComponentInChildren<ParticleSystem>().main.duration);
+        Destroy(gameObject);
+    }
+
+}
