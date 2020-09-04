b87152c111f5654e8809d7d09d78dfec56724617
Arpeus <56725663+Arpeus@users.noreply.github.com>
2020-09-04 10:41:46 +0200

    add sound

---
+++ Assets/Scripts/Audio/SoundButton.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEngine;
+
+public class SoundButton : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip audioClip;
+
+    public AudioSource OveraudioSource;
+    public AudioClip OveraudioClip;
+
+    public void playClip()
+    {
+        audioSource.clip = audioClip;
+        audioSource.Play();
+    }
+
+    public void OnMouseOver()
+    {
+        OveraudioSource.clip = OveraudioClip;
+        OveraudioSource.Play();
+    }
+}
